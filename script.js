c11bf3984e80878ada79a10ac12be91a43173cfa
Indhira T <indhirat@cognitivemobile.net>
2026-06-08 12:31:18 +0530

    Added today news

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const API_URL =
-"https://newsdata.io/api/1/latest?apikey=pub_573b266e982d4c8da436235c36d38b50&q=tamil news";
+"https://newsdata.io/api/1/latest?apikey=pub_573b266e982d4c8da436235c36d38b50&q=tamil today news";
 
 fetch(API_URL)
 .then(response => response.json())
